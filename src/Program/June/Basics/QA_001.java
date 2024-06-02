47e27ed817f6f79b3a98c82a40c64e89eff2d6f9
Rohit <rohitthakare.official@gmail.com>
2024-06-02 09:26:26 +0530

    first commit from terminal

---
+++ src/Program/June/Basics/QA_001.java
@@ -0,0 +1,26 @@
+package Program.June.Basics;
+
+import java.util.Arrays;
+
+public class QA_001 {
+    /*
+     difference between println and print
+     println will print the string and move to the next line
+     print will print the string and stay in the same line
+     */
+
+    /*
+    JVM look for main method to start the execution
+    JVM will look for System.out.println() to print the output
+    JVM -> load the class -> execute the main method --> execute the statement line by line.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World\n");
+        System.out.print("Hello Rohit");
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("HelloWorld.main");
+        System.out.println("Hello world");
+        System.out.println(2+2);
+    }
+}
---
+++ src/Program/June/Basics/QA_002.java
@@ -0,0 +1,26 @@
+package Program.June.Basics;
+
+public class QA_002 {
+    //keywords
+    //package, class, public, static, void, main, String, args, System, out, println, print
+
+    public static void main(String[] args) {
+
+        // Variables - Value for the this can be changed in the program.
+        // name -> pramod
+        // name -> lucky
+        // name -> dutta
+
+        //Mother -> [] - sugar ->
+        int age = 65;
+        // Integer type of container storing 65 as value in the variable name-> age
+        age = -1;
+        age = age +1;
+        System.out.println(age);
+
+
+        //vairiables -> means value can be changes thats why variable
+        // if you store any value then if you update the value of variable it will store the new value
+
+    }
+}
